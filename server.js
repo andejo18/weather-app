32fbe84b2ae617b019304e9fabaec95840edde9f
Joel Anderson <andejo18@gmail.com>
2019-02-02 10:24:22 -0500

    Add in node-persist, with some POC tests. async added. nvm 8.15.0

--- server.js
+++ server.js
@@ -3,38 +3,60 @@ const bodyParser = require('body-parser');
 const request = require('request');
 const app = express();
 
+const storage = require('node-persist');
+
+
 const apiKey = '48febd2e7ae455a1b2e8ab15eef19f3b';
 
-app.use(express.static('public'));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.set('view engine', 'ejs');
-
-app.get('/', function (req, res) {
-  res.render('index', {weather: null, error: null});
-});
 
 
-app.post('/', function (req, res) {
-  let city = req.body.city;
-  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+(async () => {
+  await storage.init();
 
-  request(url, function (err, response, body) {
-    if(err){
-      res.render('index', {weather: null, error: 'Error, please try again'});
+  app.use(express.static('public'));
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.set('view engine', 'ejs');
+
+  app.get('/', function (req, res) {
+    res.render('index', {weather: null, error: null});
+  });
+
+
+  app.post('/', async function (req, res) {
+    let city = req.body.city;
+    let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+
+    console.log(`checking database for ${city}`);
+    let storedItem = await storage.getItem(city);
+
+    if (! storedItem) {
+      console.log(`${city} not found. Setting value in database.`);
+      await storage.setItem(city, 'YAYAYAY');
     } else {
-      let weather = JSON.parse(body);
-      console.log(weather.main);
+      console.log(`${city} found!`);
+      console.log(storedItem);
+    }
 
-      if(weather.main == undefined){
+    /*
+    request(url, function (err, response, body) {
+      if(err){
         res.render('index', {weather: null, error: 'Error, please try again'});
       } else {
-        let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
-        res.render('index', {weather: weatherText, error: null});
-      }
-    }
-  });
-});
+        let weather = JSON.parse(body);
+        console.log(weather.main);
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
+        if(weather.main == undefined){
+          res.render('index', {weather: null, error: 'Error, please try again'});
+        } else {
+          let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+          res.render('index', {weather: weatherText, error: null});
+        }
+      }
+    }); */
+  });
+
+  app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+  });
+})();
+
