c7053fac83de560c8888cf348d43db50ff4d90e7
Joel Anderson <andejo18@gmail.com>
2019-02-02 11:05:30 -0500

    Remove database stuff for a hot second

--- server.js
+++ server.js
@@ -7,8 +7,10 @@ const storage = require('node-persist');
 
 
 const apiKey = '48febd2e7ae455a1b2e8ab15eef19f3b';
+/* 
+const callWeather = async function() {
 
-
+} */
 
 (async () => {
   await storage.init();
@@ -26,6 +28,7 @@ const apiKey = '48febd2e7ae455a1b2e8ab15eef19f3b';
     let city = req.body.city;
     let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
 
+/*
     console.log(`checking database for ${city}`);
     let storedItem = await storage.getItem(city);
 
@@ -44,8 +47,9 @@ const apiKey = '48febd2e7ae455a1b2e8ab15eef19f3b';
 
       console.log(storedItem);
     }
+*/
 
-    /*
+
     request(url, function (err, response, body) {
       if(err){
         res.render('index', {weather: null, error: 'Error, please try again'});
@@ -60,11 +64,10 @@ const apiKey = '48febd2e7ae455a1b2e8ab15eef19f3b';
           res.render('index', {weather: weatherText, error: null});
         }
       }
-    }); */
+    });
   });
 
   app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
   });
 })();
-
