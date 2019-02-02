5abb49d5e42b4e3922e1a5b7976b8ca19da025f5
Joel Anderson <andejo18@gmail.com>
2019-02-02 11:29:14 -0500

    Learn something/refactor

--- server.js
+++ server.js
@@ -7,10 +7,31 @@ const storage = require('node-persist');
 
 
 const apiKey = '48febd2e7ae455a1b2e8ab15eef19f3b';
-/* 
-const callWeather = async function() {
 
-} */
+const callWeather = function(city) {
+  console.log(`callWeather called, with ${city}!`);
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+  return url;
+/*
+  request(url, function (err, response, body) {
+    return 
+
+    
+    if(err){
+      res.render('index', {weather: null, error: 'Error, please try again'});
+    } else {
+      let weather = JSON.parse(body);
+      console.log(weather.main);
+
+      if(weather.main == undefined){
+        res.render('index', {weather: null, error: 'Error, please try again'});
+      } else {
+        let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+        res.render('index', {weather: weatherText, error: null});
+      }
+    }
+  }); */
+};
 
 (async () => {
   await storage.init();
@@ -26,7 +47,7 @@ const callWeather = async function() {
 
   app.post('/', async function (req, res) {
     let city = req.body.city;
-    let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+    console.log(`post with ${city} called`);
 
 /*
     console.log(`checking database for ${city}`);
@@ -48,23 +69,10 @@ const callWeather = async function() {
       console.log(storedItem);
     }
 */
+    let newurl = callWeather(city);
+    console.log(newurl);
 
-
-    request(url, function (err, response, body) {
-      if(err){
-        res.render('index', {weather: null, error: 'Error, please try again'});
-      } else {
-        let weather = JSON.parse(body);
-        console.log(weather.main);
-
-        if(weather.main == undefined){
-          res.render('index', {weather: null, error: 'Error, please try again'});
-        } else {
-          let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
-          res.render('index', {weather: weatherText, error: null});
-        }
-      }
-    });
+    
   });
 
   app.listen(3000, function () {
